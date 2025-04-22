16f3bc6101d18b037ccb9cc9e33a7a75341301ab
Jeya Jothi <jeyajothi108@gmail.com>
2025-04-22 15:13:55 +0530

    username changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const pool = new Pool({
   host: process.env.DB_Host,
-  port: process.quitenv.DB_PORT,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
