a70b32eec53c72ae73c79efbf4ff58d1c21ed0de
Jeya Jothi <jeyajothi108@gmail.com>
2025-04-22 16:46:05 +0530

    DB_Host to DB_HOST

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 const pool = new Pool({
-  host: process.env.DB_Host,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
